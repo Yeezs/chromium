fae7a5ff1360c01281bf79b600677246ee96a3e6
Yeezs <yeezsyt@gmail.com>
2023-02-23 18:13:57 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace chromium
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.chromiumWebBrowser1 = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.Go = new System.Windows.Forms.ToolStripButton();
+            this.searchbox = new System.Windows.Forms.ToolStripTextBox();
+            this.BrowserTabs = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.addBrowser = new System.Windows.Forms.ToolStripButton();
+            this.removeBrowserTab = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.BrowserTabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chromiumWebBrowser1
+            // 
+            this.chromiumWebBrowser1.ActivateBrowserOnCreation = false;
+            this.chromiumWebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chromiumWebBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.chromiumWebBrowser1.Name = "chromiumWebBrowser1";
+            this.chromiumWebBrowser1.Size = new System.Drawing.Size(800, 450);
+            this.chromiumWebBrowser1.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Go,
+            this.addBrowser,
+            this.removeBrowserTab,
+            this.searchbox});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 32);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Go
+            // 
+            this.Go.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.Go.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.Go.Image = ((System.Drawing.Image)(resources.GetObject("Go.Image")));
+            this.Go.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Go.Name = "Go";
+            this.Go.Size = new System.Drawing.Size(40, 29);
+            this.Go.Text = "Go";
+            this.Go.Click += new System.EventHandler(this.Go_Click);
+            // 
+            // searchbox
+            // 
+            this.searchbox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.searchbox.Name = "searchbox";
+            this.searchbox.Size = new System.Drawing.Size(100, 32);
+            // 
+            // BrowserTabs
+            // 
+            this.BrowserTabs.Controls.Add(this.tabPage1);
+            this.BrowserTabs.Controls.Add(this.tabPage2);
+            this.BrowserTabs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BrowserTabs.Location = new System.Drawing.Point(0, 32);
+            this.BrowserTabs.Name = "BrowserTabs";
+            this.BrowserTabs.SelectedIndex = 0;
+            this.BrowserTabs.Size = new System.Drawing.Size(800, 418);
+            this.BrowserTabs.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 392);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // addBrowser
+            // 
+            this.addBrowser.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.addBrowser.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.addBrowser.Image = ((System.Drawing.Image)(resources.GetObject("addBrowser.Image")));
+            this.addBrowser.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.addBrowser.Name = "addBrowser";
+            this.addBrowser.Size = new System.Drawing.Size(29, 29);
+            this.addBrowser.Text = "+";
+            this.addBrowser.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // removeBrowserTab
+            // 
+            this.removeBrowserTab.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.removeBrowserTab.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.removeBrowserTab.Image = ((System.Drawing.Image)(resources.GetObject("removeBrowserTab.Image")));
+            this.removeBrowserTab.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.removeBrowserTab.Name = "removeBrowserTab";
+            this.removeBrowserTab.Size = new System.Drawing.Size(24, 29);
+            this.removeBrowserTab.Text = "-";
+            this.removeBrowserTab.Click += new System.EventHandler(this.removeBrowserTab_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BrowserTabs);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.chromiumWebBrowser1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.BrowserTabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CefSharp.WinForms.ChromiumWebBrowser chromiumWebBrowser1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton Go;
+        private System.Windows.Forms.ToolStripTextBox searchbox;
+        private System.Windows.Forms.TabControl BrowserTabs;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolStripButton addBrowser;
+        private System.Windows.Forms.ToolStripButton removeBrowserTab;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chromium
+{
+    public partial class Form1 : Form
+    {
+        ChromiumWebBrowser chromiumBrowser = null;
+        List<ChromiumWebBrowser> chromiumBrowsers = new List<ChromiumWebBrowser>();
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeBrowswer();
+        }
+
+        public void InitializeBrowswer()
+        {
+            var settings = new CefSettings();
+            Cef.Initialize(settings);
+            chromiumBrowser = new ChromiumWebBrowser("https://google.com");
+            BrowserTabs.TabPages[0].Controls.Add(chromiumBrowser);
+            chromiumBrowser.Dock = DockStyle.Fill;
+
+            chromiumBrowser = new ChromiumWebBrowser("https://google.com");
+            BrowserTabs.TabPages[1].Controls.Add(chromiumBrowser);
+            chromiumBrowser.Dock = DockStyle.Fill;
+
+        }
+
+        private void Go_Click(object sender, EventArgs e)
+        {
+            //searchbox.Load(Address.Text);
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            AddBrowserTab();
+        }
+
+        private void AddBrowserTab()
+        {
+            TabPage tabPage = new TabPage();
+            tabPage.Text = "BrowserPage";
+
+            chromiumBrowser = new ChromiumWebBrowser("https://google.com");
+            tabPage.Controls.Add(chromiumBrowser);
+            chromiumBrowser.Dock = DockStyle.Fill;
+
+            BrowserTabs.TabPages.Add(tabPage);
+        }
+
+        private void removeBrowserTab_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(BrowserTabs.TabCount > 1)
+                {
+                    BrowserTabs.TabPages.Remove(BrowserTabs.SelectedTab);
+                }
+                else
+                {
+                    AddBrowserTab();
+                }
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+    }
+}
